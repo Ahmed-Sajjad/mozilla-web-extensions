7339762e81e2f0b80ee66953346e8b672206dae7
Ahmed Sajjad <asajjad@folio3.com>
2021-08-03 22:09:57 +0500

    Code Refactoring Tasks Done -- Added regions; Event handlers and Functions -- Moved working logic out of event handlers to named functions -- Sorted functions alphabetically -- Moved variables pointing to html elements to top

--- finops-table-browser/popup/table_browser_pop.js
+++ finops-table-browser/popup/table_browser_pop.js
@@ -2,71 +2,88 @@
 const urlRegex = new RegExp(/^http(s)?:\/\/.+\.com/gm);
 const queryParamsRegex = new RegExp(/.*\?(.*)/gm);
 
-let body = document.getElementsByTagName("body")[0];
+let body      = document.getElementsByTagName("body")[0];
+let company   = document.getElementById("company");
+let tableName = document.getElementById("tableName");
 
-document.onreadystatechange = function() {
+//#region    Event handlers
+document.onreadystatechange = onDocumentReady
 
-    function getQueryParams(url) {
-        const queryString = queryParamsRegex.exec(url)[1];
+document.getElementById("submit-btn").addEventListener("click", handleSubmit);
 
-        return queryString.split('&').reduce((acc, exp) => {
-            [key, value] = exp.split('=');
-            acc[key] = value;
-            return acc;
-        }, {});
-    }
+document.getElementById("theme-toggle").addEventListener("change", toggleTheme);
+//#endregion Event handlers
 
-    if (document.readyState === "complete") {
-        browser.tabs.query({currentWindow: true,active: true}).then((t) => {
+//#region    Functions
+/**
+ * Generates the url for the table browser based on the company and table name
+ * @param {string} dynamicsUrl The URL of the Finance and Operations (D365) site
+ * @returns 
+ */
+ function createTableBrowserTab(dynamicsUrl) {
+    const finopsUrl         = dynamicsUrl.match(urlRegex)[0];
+    const companyValue      = company.value;
+    const tableNameValue    = tableName.value;
+
+    if (!finopsUrl || !company || !tableName) return;
+
+    var urlTemplate = "{{finopsUrl}}/?cmp={{company}}&mi=SysTableBrowser&tableName={{tableName}}";
+
+    urlTemplate = urlTemplate.replace("{{finopsUrl}}", finopsUrl)
+                             .replace("{{company}}", companyValue)
+                             .replace("{{tableName}}", tableNameValue);
+
+    browser.tabs.create({ url: urlTemplate });
+}
+
+/**
+ * Extracts the query params from the provided url
+ * @param {string} url 
+ * @returns 
+ */
+function getQueryParams(url) {
+    const queryString = queryParamsRegex.exec(url)[1];
+
+    return queryString.split('&').reduce((acc, exp) => {
+        [key, value] = exp.split('=');
+        acc[key] = value;
+        return acc;
+    }, {});
+}
+
+/**
+ * Handles the submit button event
+ * @param {Event} event 
+ */
+function handleSubmit(event) {
+    browser.tabs.query({ currentWindow: true, active: true })
+                .then((t) => createTableBrowserTab(t[0].url));
+}
+
+/**
+ * Function that performs operations when the document is ready 
+ */
+ function onDocumentReady() {
+    if (document.readyState === "complete")
+    {
+        browser.tabs.query({ currentWindow: true,active: true }).then((t) => {
             var url = t[0].url;
             var queryParams = getQueryParams(url);
-            console.log(queryParams);
 
             const currentCompany = queryParams["cmp"];
 
-            const company = document.getElementById("company");
-
             company.value = currentCompany ?? "";
-
         });
     }
 }
 
-document.getElementById("submit-btn").addEventListener("click", function(e) {
+/**
+ * Toggles the theme from light to dark or vice versa
+ * @param {Event} event 
+ */
+function toggleTheme(event) {
+    let isDark = event.target.checked; 
     
-    function createTableBrowserTab(dynamicsUrl) {
-        const finopsUrl = dynamicsUrl.match(urlRegex)[0];
-        const company = document.getElementById("company").value;
-        const tableName = document.getElementById("tableName").value;
-    
-        console.log(finopsUrl);
-        console.log(company);
-        console.log(tableName);
-    
-        if (!finopsUrl || !company || !tableName) {
-            return;
-        }
-    
-        var urlTemplate = "{{finopsUrl}}/?cmp={{company}}&mi=SysTableBrowser&tableName={{tableName}}";
-    
-        urlTemplate = urlTemplate.replace("{{finopsUrl}}", finopsUrl)
-                                 .replace("{{company}}", company)
-                                 .replace("{{tableName}}", tableName);
-    
-        console.log(urlTemplate);
-    
-        browser.tabs.create({
-            url: urlTemplate
-        });
-    }
-
-    browser.tabs.query({currentWindow: true, active: true}).then((t) => {
-        createTableBrowserTab(t[0].url);
-    });
-});
-
-document.getElementById("theme-toggle").addEventListener("change", function(e) {
-    let isDark = e.target.checked; 
-    console.log(isDark);
     isDark ? body.setAttribute("data-theme", "dark") : body.removeAttribute("data-theme");
-});
+}
+//#endregion Functions
